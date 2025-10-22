c62683f399a78aa81e35f497ff2e86c86f1d0bec
Collins Kibet <kollcibe05@gmail.com>
2025-10-22 11:34:48 +0300

    Revert "AddedDeliverablesexcludingremoveBotProfile"
    This reverts commit d5b8b9f9812cc77af254989e4fa5f441de90d24f.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react" 
-import MyBotArmy from "./MyBotArmy";
+import React, {useState, useEffect} from "react" 
+
+function GetTitles () {
+  const [titles, setTitles] = useState([])
+
+useEffect ( () => {  
+   fetch ("http://localhost:3000/bots") 
+  .then(res => res.json())
+  .then(data => {
+ const fetchedTitles = data.map( robot => robot.name)
+ setTitles(fetchedTitles)
+  })
+}, [])
+return <ul>{titles.map((title, index) => <li className='robotnames' key={index}>{title}</li>)}</ul> 
+
+ 
+}
 function App() {
   return (
     <div className="App">
-      <h1 id="maintitle">BOT BATTLR👾</h1>
+      <h1>BOT BATTLR</h1>
        <div>
-      <MyBotArmy/>
+        <GetTitles/>
      </div>
     </div>
   );
